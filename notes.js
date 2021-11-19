9340e91ab886b7a2515c9357a0af7dc9e2d976bc
Caleb Thompson <calebjthompson1230@gmail.com>
2021-11-19 08:59:34 -0600

    Initial Commit

---
+++ notes.js
@@ -0,0 +1,14 @@
+const notes = [
+    {
+        subject: "July 4th",
+        date: "July 4th, 2081",
+        feeling: "patriotic",
+        timeSpent: "30 minutes",
+    },
+    {
+        subject: "Halloween",
+        date: "October 31st, 2081",
+        feeling: "spooky",
+        timeSpent: "5 hours"
+    },
+]
